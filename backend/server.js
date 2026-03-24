6517653872fdacd66ffbff5d139e1b5141aa5db5
vamshikrishna12376 <krishna.21mis7052@vitapstudent.ac.in>
2026-03-24 21:26:22 +0530

    Updated backend code

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,16 @@ app.post("/trigger", async (req, res) => {
 
     res.json(result);
 });
+app.post("/webhook", async (req, res) => {
+    console.log("🔥 GitHub Webhook Triggered!");
 
+    const repo = req.body.repository?.name || "unknown";
+    const commit = req.body.head_commit?.id || "latest";
+
+    const result = await orchestrator.runPipeline(repo, commit);
+
+    res.json({ message: "Pipeline triggered", result });
+});
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 const cors = require("cors");app.use(cors());
